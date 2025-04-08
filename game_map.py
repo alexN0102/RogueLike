61a146834e5f4952a4d615db8ea9cca0c2e2b1cd
alexN0102 <alexander.l.novelli@gmail.com>
2025-04-08 14:17:58 -0400

    added logic to game_map for tiles explored and can see

--- game_map.py
+++ game_map.py
@@ -8,6 +8,9 @@ class GameMap:
         self.width, self.height = width, height
         self.tiles = np.full((width, height), fill_value=tile_types.wall, order="F")
         
+        self.visible = np.full((width, height), fill_value=False, order="F") # tiles the player can currently see
+        self.explored = np.full((width, height), fill_value=False, order="F") # tiles the player has explored
+        
     def in_bounds(self, x: int, y: int) -> bool:
         """Return True if x and y are inside of the bounds of this map."""
         return 0 <= x < self.width and 0 <= y < self.height

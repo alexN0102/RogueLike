0dfcff3c30ab1faf0b4bec18bd8d0acd65591095
alexN0102 <alexander.l.novelli@gmail.com>
2025-04-09 14:53:12 -0400

    implimented save game into main

--- main.py
+++ main.py
@@ -6,9 +6,14 @@ import tcod
 
 import color
 import exceptions
+import setup_game
 import input_handlers
 
-import setup_game
+def save_game(handler: input_handlers.BaseEventHandler, filename: str) -> None:
+    """If the current event handler has an active Engine then save it"""
+    if isinstance(handler, input_handlers.EventHandler):
+        handler.engine.save_as(filename)
+        print("Game saved.")
 
 def main() -> None:
     screen_width = 80
@@ -47,8 +52,10 @@ def main() -> None:
         except exceptions.QuitWithoutSaving:
             raise
         except SystemExit: # Save and quit
+            save_game(handler, "savegame.sav")
             raise
         except BaseException: # Save on any other unexpected exeption
+            save_game(handler, "savegame.sav")
             raise
             
                 

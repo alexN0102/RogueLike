7ae683f68297745414c2619f93d779795c21a5aa
alexN0102 <alexander.l.novelli@gmail.com>
2025-04-09 09:43:02 -0400

    added ability to now view history pressing v key

--- input_handlers.py
+++ input_handlers.py
@@ -96,6 +96,9 @@ class MainGameEventHandler(EventHandler):
         elif key == tcod.event.K_ESCAPE:
             action = EscapeAction(player)
             
+        elif key == tcod.event.K_v:
+            self.engine.event_handler = HistoryViewer(self.engine)
+            
         # No valid key was pressed
         return action
     
